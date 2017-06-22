849b5432dd4a30a5484fdc53d0106a40ba4b24d4
Grzegorz Bielański <gbielanski@poczta.onet.pl>
2017-06-22 14:57:24 +0200

    Drawable for monuments

--- app/src/main/java/com/zielonkatourguide/zielonkatourguide/MonumentsFragment.java
+++ app/src/main/java/com/zielonkatourguide/zielonkatourguide/MonumentsFragment.java
@@ -25,12 +25,12 @@ public class MonumentsFragment extends Fragment {
         View rootElement = inflater.inflate(R.layout.attractions_list, container, false);
         ListView listView = (ListView) rootElement.findViewById(R.id.list);
         ArrayList<Attraction> monumentsList = new ArrayList<>();
-        monumentsList.add(new Attraction(getString(R.string.monument_1)));
-        monumentsList.add(new Attraction(getString(R.string.monument_2)));
-        monumentsList.add(new Attraction(getString(R.string.monument_3)));
-        monumentsList.add(new Attraction(getString(R.string.monument_4)));
-        monumentsList.add(new Attraction(getString(R.string.monument_5)));
-        monumentsList.add(new Attraction(getString(R.string.monument_6)));
+        monumentsList.add(new Attraction(R.drawable.monument_church, getString(R.string.monument_1)));
+        monumentsList.add(new Attraction(R.drawable.monument_skwer, getString(R.string.monument_2)));
+        monumentsList.add(new Attraction(R.drawable.monument_former_hostel, getString(R.string.monument_3)));
+        monumentsList.add(new Attraction(R.drawable.monument_house, getString(R.string.monument_4)));
+        monumentsList.add(new Attraction(R.drawable.monument_plaque, getString(R.string.monument_5)));
+        monumentsList.add(new Attraction(R.drawable.monument_museum, getString(R.string.monument_6)));
         attractionsAdapter = new AttractionsAdapter(getActivity(), monumentsList);
         listView.setAdapter(attractionsAdapter);
 
